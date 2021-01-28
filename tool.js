a054fc3e7c32c884fd11e75d2e55831e92b1f303
ppalioune <ppalioune@gmail.com>
2021-01-28 14:05:04 -0500

    integration result-builder for tool-console

--- tool.js
+++ tool.js
@@ -1,9 +1,13 @@
 'use strict';
+const ResultBuilder = require('result-builder')
+
 
 class Tool {
     constructor({ page, consoleMessages }) {
         this.page = page;
         this.consoleMessages = consoleMessages;
+        //Build result by result-builder
+        this.builder = new ResultBuilder()
     }
 
     async run() {
@@ -19,37 +23,39 @@ class Tool {
                 }, 500);
             });
         });
+        this.checkErrors();
+        this.checkWarnings();
+        this.checkLogs();
     }
 
     get results() {
-        return [
-            {
-                'uniqueName': 'errors',
-                'title': 'Errors',
-                'description': 'A list of error messages appearing in the browser\'s console when visiting your page.',
-                'weight': .9,
-                'score': 1 - Math.min(this.consoleMessages.errors.length, 10) * .1,
-                'table': ['Error message'].concat(this.consoleMessages.errors).map(msg => [msg]),
-                'recommendations': this.consoleMessages.errors.length ? 'Fix the errors that appear in the browser\'s console when visiting your website.' : null
-            },
-            {
-                'uniqueName': 'warnings',
-                'title': 'Warnings',
-                'description': 'A list of warning messages appearing in the browser\'s console when visiting your page.',
-                'weight': .1,
-                'score': 1 - Math.min(this.consoleMessages.warnings.length, 10) * .1,
-                'table': ['Warning message'].concat(this.consoleMessages.warnings).map(msg => [msg]),
-            },
-            {
-                'uniqueName': 'logs',
-                'title': 'Logs and information messages',
-                'description': 'A list of benign log messages appearing in the browser\'s console when visiting your page.',
-                'weight': 0,
-                'score': 0,
-                'table': ['Message'].concat(this.consoleMessages.others).map(msg => [msg]),
-            },
-        ];
+        return this.builder.toArray()
     };
+    checkErrors(){
+        const result = this.builder.newTest("errors")
+        result.setTitle("Errors")
+            .setDescription("A list of error messages appearing in the browser\'s console when visiting your page.")
+            .setWeight(.9)
+            .setScore(1 - Math.min(this.consoleMessages.errors.length, 10) * .1)
+            .addRecommendation(this.consoleMessages.errors.length ? 'Fix the errors that appear in the browser\'s console when visiting your website.' : null)
+            .addTableRows(['Error message'].concat(this.consoleMessages.errors).map(msg => [msg]))
+    }
+    checkWarnings(){
+        const result = this.builder.newTest("warnings")
+        result.setTitle("warnings")
+            .setDescription("A list of warning messages appearing in the browser\'s console when visiting your page.")
+            .setWeight(.1)
+            .setScore(1 - Math.min(this.consoleMessages.errors.length, 10) * .1)
+            .addTableRows(['Warning message'].concat(this.consoleMessages.warnings).map(msg => [msg]))
+    }
+    checkLogs(){
+        const result = this.builder.newTest("logs")
+        result.setTitle("Logs")
+            .setDescription("Logs and information messages")
+            .setWeight(0)
+            .setScore(0)
+            .addTableRows(['Message'].concat(this.consoleMessages.others).map(msg => [msg]))
+    }
 
     async cleanup() {
 
